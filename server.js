ba623139db882f8f8d7c9dd979088b28d3ee4f56
Yoko Tsuda <yoko20209@gmail.com>
2021-02-01 19:16:40 +0900

    WIP trying to contact db with knex, not working

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const knex = require("./knex");
 
 function setupServer() {
     const app = express();
     app.use(express.json());
+
+    app.get("/list-of-birthstones", async (_, res) => {
+
+        const data = await knex.select().table('birthstones');
+        res.status(200)
+        res.send("heeeeeelo");
+    });
 
     return app;
 }
